78263cfbe866505968aa7251e1cfdd3d46e41bcd
Gerardo Juarez <gerardo_juarez@hotmail.com>
2022-05-04 15:19:40 -0500

    se agrego otra prueba

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -1,18 +1,34 @@
+/* Importing the User class from the User.js file. */
 const User = require('./../../app/models/User')
-
-describe("Unit Tests for User class", () => {
-
-	test('Create an User object', () => {
-    const user = new User(1, "carlogilmar", "Carlo", "Bio", "dateCreated", "lastUpdated")
-
-
-		expect(user.id).toBe(1)
-		expect(user.username).toBe("carlogilmar")
-		expect(user.name).toBe("Carlo")
-		expect(user.bio).toBe("Bio")
-        expect(user.dateCreated).not.toBeUndefined()
-        expect(user.lastUpdated).not.toBeUndefined()
-        //expect(user.dateCreated).toBe("dateCreated")
-        //expect(user.lastUpdated).toBe("lastUpdated")
-	});
+/* A unit test for the User class. */
+describe('Unit Tests for User class', () => {
+  /* A test case. */
+  test('Create an User object', () => {
+    /* Creating a new instance of the User class. */
+    const user = new User(
+      /* Setting the bio property of the User object to the value of the bio parameter. */
+      1,
+      'carlogilmar',
+      'Carlo',
+      'Bio',
+      'dateCreated',
+      'lastUpdated'
+    )
+    /* Testing the User class. */
+    expect(user.id).toBe(1)
+    expect(user.username).toBe('carlogilmar')
+    expect(user.name).toBe('Carlo')
+    expect(user.bio).toBe('Bio')
+    expect(user.dateCreated).not.toBeUndefined()
+    expect(user.lastUpdated).not.toBeUndefined()
+    //expect(user.dateCreated).toBe("dateCreated")
+    //expect(user.lastUpdated).toBe("lastUpdated")
+  });
+  test ('Add getter', () => {
+	  const user = new User(1, "carlogilmar", "Carlo", "Bio")
+	  expect(user.getUsername).toBe("carlogilmar")
+	  expect(user.getBio).toBe("Bio")
+	  expect(user.getDateCreated).not.toBeUndefined()
+	  expect(user.getLastUpdated).not.toBeUndefined()
+  });
 })
