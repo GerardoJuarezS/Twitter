e5bae77e125d28acefff8006bd3382d06e173b3e
Gerardo Juarez <gerardo_juarez@hotmail.com>
2022-05-07 14:50:13 -0500

    se agrego la prueba de la clase User.test

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -1,41 +1,40 @@
-/* Importing the User class from the User.js file. */
-const User = require('./../../app/models/User')
-/* A unit test for the User class. */
-describe('Unit Tests for User class', () => {
-  /* A test case. */
-  test('Create an User object', () => {
-    /* Creating a new instance of the User class. */
-    const user = new User(
-      /* Setting the bio property of the User object to the value of the bio parameter. */
-      1,
-      'carlogilmar',
-      'Carlo',
-      'Bio',
-      'dateCreated',
-      'lastUpdated'
-    )
-    /* Testing the User class. */
-    expect(user.id).toBe(1)
-    expect(user.username).toBe('carlogilmar')
-    expect(user.name).toBe('Carlo')
-    expect(user.bio).toBe('Bio')
+const User = require('./../../app/models/user')
+
+describe("Unit Tests for User class", () => {
+
+  test('1) Create empty object', () => {
+    const user = new User()
+    expect(user.id).toBeUndefined()
+    expect(user.username).toBeUndefined()
+    expect(user.name).toBeUndefined()
+    expect(user.bio).toBeUndefined()
     expect(user.dateCreated).not.toBeUndefined()
     expect(user.lastUpdated).not.toBeUndefined()
-    //expect(user.dateCreated).toBe("dateCreated")
-    //expect(user.lastUpdated).toBe("lastUpdated")
   })
-  test('Add getter', () => {
-    const user = new User(1, 'carlogilmar', 'Carlo', 'Bio')
-    expect(user.getUsername).toBe('carlogilmar')
-    expect(user.getBio).toBe('Bio')
+
+	test('2) Create an User object', () => {
+    const user = new User(1, "carlogilmar", "Carlo", "Bio")
+		expect(user.id).toBe(1)
+		expect(user.username).toBe("carlogilmar")
+		expect(user.name).toBe("Carlo")
+		expect(user.bio).toBe("Bio")
+	});
+
+	test('3) Add getters', () => {
+    const user = new User(1, "carlogilmar", "Carlo", "Bio")
+		expect(user.getUsername).toBe("carlogilmar")
+		expect(user.getBio).toBe("Bio")
     expect(user.getDateCreated).not.toBeUndefined()
     expect(user.getLastUpdated).not.toBeUndefined()
-  })
-  test('Add setters', () => {
-	  const user = new User(1,"carlogilmar", "carlo", "Bio")
-	  user.setUsername = "Gilmar"
-	  expect(user.username).toBe("Gilmar")
-	  user.setBio = "New bio"
-	  expect(user.bio).toBe("New bio")
-  })
+	});
+
+	test('4) Add setters', () => {
+    const user = new User(1, "carlogilmar", "Carlo", "Bio")
+    user.setUsername = "Gilmar"
+		expect(user.username).toBe("Gilmar")
+
+    user.setBio = "New bio"
+		expect(user.bio).toBe("New bio")
+	});
+
 })
